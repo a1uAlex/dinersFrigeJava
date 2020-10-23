4dcf19400f934270c6ee8f79fad5a62e02e9b73c
alsch068 <alexander.schroeder068@stud.fh-dortmund.de>
2020-10-23 16:12:12 +0200

    Add Entity, change to H2

---
+++ src/main/java/de/alex/dinersFrige/models/Inhalt.java
@@ -0,0 +1,20 @@
+package de.alex.dinersFrige.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Inhalt {
+
+    @Id
+    Long id;
+
+
+}
