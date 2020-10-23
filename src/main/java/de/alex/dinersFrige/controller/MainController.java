c93b2cabeddcd2f5713277009787a0b9af6ab62a
alsch068 <alexander.schroeder068@stud.fh-dortmund.de>
2020-10-23 18:24:47 +0200

    Add MainPage

---
+++ src/main/java/de/alex/dinersFrige/controller/MainController.java
@@ -0,0 +1,18 @@
+package de.alex.dinersFrige.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
+    public String mainPage(Model model){
+        model.addAttribute("message", "Dies ist ein Test!");
+        return "main";
+    }
+
+
+}
