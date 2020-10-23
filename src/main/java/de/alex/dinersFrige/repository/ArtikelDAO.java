5634edaaa903e2fe98e31f664f22a4e6e3487559
alsch068 <alexander.schroeder068@stud.fh-dortmund.de>
2020-10-23 17:34:16 +0200

    AddArtikelDAO

---
+++ src/main/java/de/alex/dinersFrige/repository/ArtikelDAO.java
@@ -0,0 +1,8 @@
+package de.alex.dinersFrige.repository;
+
+import de.alex.dinersFrige.models.Artikel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtikelDAO extends JpaRepository<Artikel, Long> {
+
+}
