ae46157e31da2431ce18a01ac3b612e02ab1faae
alsch068 <alexander.schroeder068@stud.fh-dortmund.de>
2020-10-23 15:32:29 +0200

    Initial commit

---
+++ src/main/java/de/alex/dinersFrige/DinersFrigeApplication.java
@@ -0,0 +1,13 @@
+package de.alex.dinersFrige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinersFrigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinersFrigeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/alex/dinersFrige/DinersFrigeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.alex.dinersFrige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinersFrigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
